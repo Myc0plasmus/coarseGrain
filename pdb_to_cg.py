f5f4aa02e7d46b1778f1e1f9af584a5994ff1e8d
Myc0plasmus <jablonski.natan@gmail.com>
2024-11-21 12:21:59 +0000

    updated tasks and structure loading works

--- pdb_to_cg.py
+++ pdb_to_cg.py
@@ -1,10 +1,19 @@
 import argparse
-from Bio.PDB.PDBList import PDBList
 from Bio import PDB
+import os
 
-argParser = argparse.ArgumentParser(description="This script takes an input pdb file in full atom representation and saves to an output file path its coarse grain representation")
+argParser = argparse.ArgumentParser(description="This script takes an input pdb file containing structure of a nucleic acid in full atom representation and saves to an output file path its coarse grain representation")
 argParser.add_argument("-i", "--input", dest = "inputFilePath" , required=True, help="input pdb file to be transformed into coarse grain representation", type=str)
 argParser.add_argument("-o", "--output", dest = "outputFilePath" , required=True, help="path for output pdb file with in coarse grain format", type=str)
 
 
 args = argParser.parse_args()
+
+pdb_parser = PDB.PDBParser()
+
+structureId = os.path.splitext(os.path.basename(args.inputFilePath))[0]
+fullAtomStructure = pdb_parser.get_structure(structureId,args.inputFilePath)
+
+print(fullAtomStructure.id)
+
+coarseGrainStructure = PDB.Structure.Structure("430d_cg")
