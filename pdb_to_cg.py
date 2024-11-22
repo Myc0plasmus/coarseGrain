08b703c2a7ac48d8abfa782f022a374e7917fd6c
Myc0plasmus <jablonski.natan@gmail.com>
2024-11-22 00:16:41 +0000

    completed transformation to coarse_grain

--- pdb_to_cg.py
+++ pdb_to_cg.py
@@ -14,6 +14,35 @@ pdb_parser = PDB.PDBParser()
 structureId = os.path.splitext(os.path.basename(args.inputFilePath))[0]
 fullAtomStructure = pdb_parser.get_structure(structureId,args.inputFilePath)
 
-print(fullAtomStructure.id)
 
-coarseGrainStructure = PDB.Structure.Structure("430d_cg")
+coarseGrainStructure = PDB.Structure.Structure(structureId + "_cg")
+
+for model in fullAtomStructure:
+    newModel = PDB.Model.Model(model.id)
+    coarseGrainStructure.add(newModel)
+    for chain in model:
+        newChain = PDB.Chain.Chain(chain.id)
+        newModel.add(newChain)
+        for i,res in enumerate(chain):
+            if not res.get_resname() in ["A","C","G","U"]:
+                continue
+            newResidue = PDB.Residue.Residue((" ", i+1, " "),res.get_resname(), " ")
+            newChain.add(newResidue)
+            # print(res.get_resname())
+            # for atom in res.get_atoms():
+            #     print(atom.get_name())
+            if "P" in res:
+                newResidue.add(res["P"])
+            newResidue.add(res["C4'"])
+            if res.get_resname() in ["U","C"]:
+                newResidue.add(res["N1"])
+                newResidue.add(res["C2"])
+                newResidue.add(res["C4"])
+            else:
+                newResidue.add(res["N9"])
+                newResidue.add(res["C2"])
+                newResidue.add(res["C6"])
+
+io = PDB.PDBIO()
+io.set_structure(coarseGrainStructure)
+io.save(args.outputFilePath)
